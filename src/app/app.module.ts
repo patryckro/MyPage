a2d55a18d8f83457093e5d1da4f767b26c95adb0
patryckro <patryckmoulaz@gmail.com>
2022-06-07 00:34:13 -0300

    my start to future

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavegationBarComponent } from './navegation-bar/navegation-bar.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegationBarComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule
